b4bd2d323c8443f30e66dcf03d0c13a584442a0f
Mark-GokRack <gok.qooma@gmail.com>
2025-11-19 13:53:12 +0900

    Update app icons and colors; migrate to adaptive icons and improve icon resources

--- app/src/main/res/icon_convert.sh
+++ app/src/main/res/icon_convert.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
-convert icon.png -resize 48x48 mipmap-mdpi/icon.png
-convert icon.png -resize 72x72 mipmap-hdpi/icon.png
-convert icon.png -resize 96x96 mipmap-xhdpi/icon.png
-convert icon.png -resize 144x144 mipmap-xxhdpi/icon.png
-convert icon.png -resize 192x192 mipmap-xxxhdpi/icon.png
+convert icon.png -resize 48x48 mipmap-mdpi/ic_launcher.webp
+convert icon.png -resize 72x72 mipmap-hdpi/ic_launcher.webp
+convert icon.png -resize 96x96 mipmap-xhdpi/ic_launcher.webp
+convert icon.png -resize 144x144 mipmap-xxhdpi/ic_launcher.webp
+convert icon.png -resize 192x192 mipmap-xxxhdpi/ic_launcher.webp
 
-convert icon_round.png -resize 48x48 mipmap-mdpi/icon_round.png
-convert icon_round.png -resize 72x72 mipmap-hdpi/icon_round.png
-convert icon_round.png -resize 96x96 mipmap-xhdpi/icon_round.png
-convert icon_round.png -resize 144x144 mipmap-xxhdpi/icon_round.png
-convert icon_round.png -resize 192x192 mipmap-xxxhdpi/icon_round.png
+convert icon_round.png -resize 48x48 mipmap-mdpi/ic_launcher_round.webp
+convert icon_round.png -resize 72x72 mipmap-hdpi/ic_launcher_round.webp
+convert icon_round.png -resize 96x96 mipmap-xhdpi/ic_launcher_round.webp
+convert icon_round.png -resize 144x144 mipmap-xxhdpi/ic_launcher_round.webp
+convert icon_round.png -resize 192x192 mipmap-xxxhdpi/ic_launcher_round.webp
+
+convert icon_foreground.png -resize 108x108 mipmap-mdpi/ic_launcher_foreground.webp
+convert icon_foreground.png -resize 162x162 mipmap-hdpi/ic_launcher_foreground.webp
+convert icon_foreground.png -resize 216x216 mipmap-xhdpi/ic_launcher_foreground.webp
+convert icon_foreground.png -resize 324x324 mipmap-xxhdpi/ic_launcher_foreground.webp
+convert icon_foreground.png -resize 432x432 mipmap-xxxhdpi/ic_launcher_foreground.webp
