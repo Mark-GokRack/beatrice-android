59a26c52fdbd4773b495c1a04d273b46391bc61d
Mark-GokRack <gok.qooma@gmail.com>
2025-11-30 09:42:13 +0900

    bump version name to 0.0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,7 +34,7 @@ android {
         minSdk = 30
         targetSdk = 36
         versionCode = 1
-        versionName = "0.0.2"
+        versionName = "0.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
